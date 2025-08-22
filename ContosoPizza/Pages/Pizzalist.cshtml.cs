7341db549c432730f6a8a53042f17c87af446100
K0W415KI <jonatanfedele@gmail.com>
2025-08-22 08:52:18 -0300

    Update de datos

---
+++ ContosoPizza/Pages/Pizzalist.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+
+namespace ContosoPizza.Pages
+{
+    public class PizzalistModel : PageModel
+    {
+        private readonly PizzaService _service;
+        public IList<Pizza> PizzaList { get; set; } = default!;
+
+        public PizzalistModel(PizzaService service)
+        {
+            _service = service;
+        }
+
+        public void OnGet()
+        {
+            PizzaList = _service.GetPizzas();
+        }
+    }
+}
